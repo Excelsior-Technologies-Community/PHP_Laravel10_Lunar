36655a76d25a92eaf1737be09329a296dc81ab90
savasanisujal1 <savasanisujal1@gmail.com>
2026-05-22 18:13:29 +0530

    implemented live search functionality for products using Livewire

---
+++ resources/views/components/theme-toggle.blade.php
@@ -0,0 +1,4 @@
+<button onclick="document.documentElement.classList.toggle('dark')" 
+        class="px-4 py-2 bg-gray-800 text-white dark:bg-yellow-500 dark:text-black rounded-lg transition-all">
+    Toggle Dark Mode
+</button>
